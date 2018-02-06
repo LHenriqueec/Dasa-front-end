da1a6d3dc4ed4ebc0b87d1a01c3c681f7dfe768c
Luiz Henrique <luizhenrique.ecl@gmail.com>
2018-02-06 21:24:48 -0200

    Realculando total dos Produtos quando detectado uma alteração

--- src/app/components/recibo/produtos-recibo/produtos-recibo.component.ts
+++ src/app/components/recibo/produtos-recibo/produtos-recibo.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 import { Produto } from '../../../entity/produto';
 import { ProdutoService } from '../../../services/produto.service';
+import { OnChanges, DoCheck } from '@angular/core/src/metadata/lifecycle_hooks';
 
 @Component({
   selector: 'app-produtos-recibo',
@@ -9,18 +10,22 @@ import { ProdutoService } from '../../../services/produto.service';
   styleUrls: ['./produtos-recibo.component.css'],
   encapsulation: ViewEncapsulation.None
 })
-export class ProdutosReciboComponent implements OnInit {
+export class ProdutosReciboComponent implements OnInit, DoCheck {
 
   produtos: Produto[];
-  total: number = 0;
+  total: number;
 
   constructor(private produtoService: ProdutoService) { }
 
   ngOnInit() {
     this.getProdutos();
-    this.produtos.forEach(produto => this.total += produto.quantidade);
   }
 
+  ngDoCheck() {
+    this.total = 0;
+    this.produtos.forEach(produto => this.total += produto.quantidade);
+  }
+  
   getProdutos(): void {
     this.produtos = this.produtoService.getProdutos();
   }
